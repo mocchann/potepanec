30f70b59504deaaf3610efad8a9264b232663edb
mocchann <s7056i3056@gmail.com>
2021-06-07 15:23:29 +0900

    request specへの修正

---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Potepan::Products', type: :request do
+  describe "#show" do
+    let(:product) { create(:product) }
+
+    before do
+      get potepan_product_path(product.id)
+    end
+
+    it "responds successfully" do
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+
+    it "assigns @product" do
+      expect(assigns(:product)).to eq product
+    end
+
+    it "render show page" do
+      expect(response).to render_template "potepan/products/show"
+    end
+  end
+end
