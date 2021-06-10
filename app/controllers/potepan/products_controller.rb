e76e8e2fb9b0430096aecd3db4e9c9acb89f965d
mocchann <s7056i3056@gmail.com>
2021-06-10 13:33:09 +0900

    関連商品の追加とそれに伴うviewの変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,9 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_RELATED_PRODUCT_COUNT = 4
+
   def show
     @product = Spree::Product.find(params[:id])
+    @taxons = @product.taxons
+    @related_products = @product.related_products.limit(MAX_RELATED_PRODUCT_COUNT)
   end
 end
